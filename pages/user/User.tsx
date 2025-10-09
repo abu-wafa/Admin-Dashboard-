ad16e7e5e4c291b17ad74017d11cb1de3309aecb
abdo wafa <abuelwafaabdelsalam@gmail.com>
2025-10-09 15:54:07 +0300

    fix: correct casing of User.scss import in User component

--- pages/user/User.tsx
+++ pages/user/User.tsx
@@ -1,6 +1,6 @@
 import Single from "../../src/components/single/Single";
 import { singleUser } from "../../src/data";
-import "./user.scss";
+import "./User.scss";
 
 export const User = () => {
   return (
