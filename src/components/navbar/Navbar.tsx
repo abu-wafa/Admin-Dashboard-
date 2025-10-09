7468f2f767b42df11761ef793f105c3250c41690
abdo wafa <abuelwafaabdelsalam@gmail.com>
2025-10-09 13:30:15 +0300

    feat: enhance responsive styles across components

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,18 +8,18 @@ export default function Navbar() {
         <span>lamadmin</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="search" className="icon" />
-        <img src="/app.svg" alt="app" className="icon" />
-        <img src="/expand.svg" alt="expand" className="icon" />
+        <img src="/search.svg" alt="search" className="icon exp" />
+        <img src="/app.svg" alt="app" className="icon exp" />
+        <img src="/expand.svg" alt="expand" className="icon exp" />
         <div className="notification">
           <img src="/notifications.svg" alt="notifications" className="icon" />
           <span className="">1</span>
         </div>
         <div className="admin">
           <img src="/user.png" alt="user" className="icon" />
-          <span>Abdo</span>
+          <span className="exp">Abdo</span>
         </div>
-        <img src="/settings.svg" alt="settings" className="icon" />
+        <img src="/settings.svg" alt="settings" className="icon exp" />
       </div>
     </div>
   );
