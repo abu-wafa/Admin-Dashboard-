b077461d8f4825c8da5354a7e527eb5c8c011d4a
abdo wafa <abuelwafaabdelsalam@gmail.com>
2025-10-13 17:12:10 +0300

    feat: add Context provider for global state management

---
+++ src/context.tsx
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+
+interface AppContextType {
+  Base_url: string;
+}
+interface ContextProps {
+  children?: React.ReactNode;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const Context: React.FC<ContextProps> = ({ children }) => {
+  const Base_url = "http://localhost:8800/api";
+  return (
+    <AppContext.Provider value={{ Base_url }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
+export default Context;
